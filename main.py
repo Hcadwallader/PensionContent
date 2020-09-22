67219f288ce14d665fcdcc4cf1861694bdce0f5e
Helen Schwan <helenschwan3@gmail.com>
2020-09-22 18:42:00 +0100

    removing unnecessary function

--- main.py
+++ main.py
@@ -14,27 +14,6 @@ def load_data():
                 fundList[item['name']][h['name']] = h['weight']
     return holdingList, fundList
 
-def process_data_works_for_one_loop(holdingList, fundList):
-    newHoldingList = {}
-    print("holdingList = {!s}".format(holdingList))
-    for holdings in holdingList:
-        print("holdings = {!s}".format(holdings))
-        if holdings in fundList:
-            print("fundList = {!s}".format(fundList))
-            replacementList = fundList[holdings]
-            print("replacementList = {!s}".format(replacementList))
-            for h in replacementList:
-                print("h = {!s}".format(h))
-                existingValue = 0
-                if h in newHoldingList:
-                    existingValue = newHoldingList[h]
-
-                newHoldingList[h] = existingValue + (holdingList[holdings] * replacementList[h])
-
-    print("newHoldingList = {!s}".format(newHoldingList))
-
-
-
 def process_data(holdingList, fundList):
     newHoldingList = {}
     print("original holdingList = {!s}".format(holdingList))
@@ -47,7 +26,6 @@ def process_data(holdingList, fundList):
             replacementList = fundList[holdings]
             print("replacementList = {!s}".format(replacementList))
             for replacement in replacementList:
-                # print("h = {!s}".format(h))
                 existingValue = 0
                 if replacement in newHoldingList:
                     existingValue = newHoldingList[replacement]
@@ -63,7 +41,7 @@ def process_data(holdingList, fundList):
             newHoldingList[holdings] = existingValue + holdingList[holdings]
 
         print("newHoldingList at end of loop= {!s}".format(newHoldingList))
-                
+
     print("newHoldingList at end of function = {!s}".format(newHoldingList))
     if anything_replaced:
         return process_data(newHoldingList, fundList)
